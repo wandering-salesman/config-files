a7d285c23e6f0305d14cf9837ef2f72250ed481f
aditya <jaiswal.aditya.deelip@gmail.com>
2024-11-09 00:49:35 +0530

    bash file for setup

---
+++ setup.sh
@@ -0,0 +1,28 @@
+# /usr/bin/bash
+
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update && sudo apt-get install spotify-client
+
+
+sudo apt-add-repository ppa:mmk2410/intellij-idea
+sudo apt-get update
+
+sudo apt-get install intellij-idea-community
+
+
+sudo rm -rf /opt/Postman
+tar -C /tmp/ -xzf <(curl -L https://dl.pstmn.io/download/latest/linux64) && sudo mv /tmp/Postman /opt/
+
+sudo tee /usr/share/applications/postman.desktop << END
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=/opt/Postman/Postman
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+END
+
