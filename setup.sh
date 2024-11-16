7a447b50fb2057afd6d7d13a3f2225fb645b689b
aditya <jaiswal.aditya.deelip@gmail.com>
2024-11-16 12:49:20 +0530

    add minikube

--- setup.sh
+++ setup.sh
@@ -26,3 +26,10 @@ Type=Application
 Categories=Development;
 END
 
+
+# install and setup auto-complete for minikube
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+minikube completion bash | sudo tee /etc/bash_completion.d/minikube > /dev/null && source ~/.bashrc
+
